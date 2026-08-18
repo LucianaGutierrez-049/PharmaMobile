01decace31e23fd286c76ed89d9a740bc1937099
LucianaGutierrez-049 <lucianagutierrezq40@gmail.com>
2026-08-18 17:33:24 -0500

    Modelos de dominio y test

---
+++ shared/src/commonTest/kotlin/pe/edu/upeu/pharmamobilee/domain/ClienteTest.kt
@@ -0,0 +1,26 @@
+package pe.edu.upeu.pharmamobilee.domain
+
+import pe.edu.upeu.pharmamobilee.domain.model.Cliente
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ClienteTest{
+
+
+    @Test
+    fun probarCliente(){
+
+        val cliente = Cliente(
+        id = 1L,
+        nombre = "Farmacia Nueva Vida",
+        correo = "ventas@central.pe",
+        telefono = "989789123"
+        )
+          val resultado = cliente.obtenerTelefono()
+
+        assertEquals(
+            "989789123",
+            resultado
+        )
+    }
+}
