8520b3b293db0473774bc3ea378f2da627fce55a
LucianaGutierrez-049 <lucianagutierrezq40@gmail.com>
2026-08-25 16:52:27 -0500

    Implementar formulario de registro de productos y cliente

--- shared/src/commonMain/kotlin/pe/edu/upeu/pharmamobilee/App.kt
+++ shared/src/commonMain/kotlin/pe/edu/upeu/pharmamobilee/App.kt
@@ -15,35 +15,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.jetbrains.compose.resources.painterResource
+import pe.edu.upeu.pharmamobilee.presentacion.cliente.ClienteScreen
+import pe.edu.upeu.pharmamobilee.presentacion.producto.ProductoScreen
 
 import pharmamobilee.shared.generated.resources.Res
 import pharmamobilee.shared.generated.resources.compose_multiplatform
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
-    }
+    ProductoScreen()
 }
